ae58f2c48088f82e4467e6db4630b5db2ba79dbb
Enerixsro <jiri.cecka@enerix.cz>
2026-05-13 16:16:56 +0200

    Add Sklik retargeting code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,49 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://c.seznam.cz/js/rc.js"
+        strategy="afterInteractive"
+      />
+
+      <Script
+        id="sklik-retargeting"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function fireSklikRetargeting() {
+              function run() {
+                if (
+                  window.sznIVA &&
+                  window.sznIVA.IS &&
+                  window.rc &&
+                  window.rc.retargetingHit
+                ) {
+                  window.sznIVA.IS.updateIdentities({
+                    eid: null
+                  });
+
+                  var retargetingConf = {
+                    rtgId: 1688154,
+                    consent: 0
+                  };
+
+                  window.rc.retargetingHit(retargetingConf);
+                } else {
+                  setTimeout(run, 300);
+                }
+              }
+
+              run();
+            })();
+          `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
